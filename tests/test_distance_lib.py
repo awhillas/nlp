9047f72717bb6550b06fe1d31f66a9b65f595a18
Alexander Whillas <whillas@gmail.com>
2015-03-28 19:25:11 +1100

    test Distance lib

--- tests/test_distance_lib.py
+++ tests/test_distance_lib.py
@@ -1,1 +1,12 @@
-__author__ = 'alex'
+import pytest
+from lib.distance import *
+
+def test_Levenshtein():
+	""" Levenshtein distance test
+	"""
+	assert optimalAlignmentDistance('123abc', '123abc') == 0
+	assert optimalAlignmentDistance('abort', 'abortt') == 1, "Inerts are not counted correctly"
+	assert optimalAlignmentDistance('abortt', 'abort') == 1, "Deletes not counted correctly"
+	assert optimalAlignmentDistance('bag', 'bog') == 1, "Replacements not counted correctly"
+	assert optimalAlignmentDistance('best', 'ebst'), "Transposition not counted correctly"
+	assert optimalAlignmentDistance('which', 'witch') == 2, "Combo insert and replace"
