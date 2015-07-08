8ef1a5356fd0108117988e5cee3ee933d2835ec7
Alexander Whillas <whillas@gmail.com>
2015-07-08 15:07:54 +1000

    restored the MEMMTag script to just one Reg. value

--- dis/MemmTag.py
+++ dis/MemmTag.py
@@ -39,25 +39,31 @@ class MemmTag(MachineLearningModule):
 
 
 		http_server = None
-		#reg = self.get('regularization')
+		reg = self.get('regularization')
 		data = ConlluReader(self.get('uni_dep_base'), '.*\.conllu')  # Corpus
-		for reg in [0.33, 0.66, 0.9, 1.0]:
-			cluster = dispy.JobCluster(compute, setup=setup, reentrant=True)
-			http_server = dispy.httpd.DispyHTTPServer(cluster) # monitor cluster at http://localhost:8181
-			jobs = []
-			unlabeled = data.sents(self.get('cv_file'))
-			for i, sentence in enumerate(unlabeled):
-				job = cluster.submit(self.dir('working'), reg, sentence)
-				job.id = i
-				jobs.append(job)
+		cluster = dispy.JobCluster(compute, setup=setup, reentrant=True)
+		http_server = dispy.httpd.DispyHTTPServer(cluster) # monitor cluster at http://localhost:8181
+		jobs = []
+		unlabeled = data.sents(self.get('cv_file'))
+		for i, sentence in enumerate(unlabeled):
+			job = cluster.submit(self.dir('working'), reg, sentence)
+			job.id = i
+			jobs.append(job)
 
-			if http_server is not None:
-				cluster.wait() # wait for all jobs to finish
-				cluster.stats()
-				http_server.shutdown() # this waits until browser gets all updates
-				cluster.close()
+		if http_server is not None:
+			cluster.wait() # wait for all jobs to finish
+			cluster.stats()
+			http_server.shutdown() # this waits until browser gets all updates
+			cluster.close()
 
-			tags = save_data(jobs)
-			self.backup(tags, self.dir('working') + '/memm_tagged_sentences-reg_%.2f.pickle' % reg)
+		tags = save_data(jobs)
+		self.backup(tags, self.dir('working') + '/memm_tagged_sentences-reg_%.2f.pickle' % reg)
 
 		return False
+
+	def load(self, path = None, filename_prefix = ''):
+		reg = self.get('regularization')
+		self.tagger = MaxEntMarkovModel(feature_templates=Ratnaparkhi96Features, word_normaliser=CollinsNormalisation)
+		self.tagger.load(self.dir('working'), '-reg_%.2f' % reg)
+		self.labeled_sequences = self.restore(self.dir('working') + '/memm_tagged_sentences-reg_%.2f.pickle' % reg)
+		return 0
