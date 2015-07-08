0a41ee6b57dc08883bc273b086cea86298ccfd77
Alexander Whillas <whillas@gmail.com>
2015-07-08 15:41:34 +1000

    Split Multi tagging 10-fold into separate Training and Tagging scripts.

--- dis/MemmMultiTrain.py
+++ dis/MemmMultiTrain.py
@@ -13,19 +13,10 @@ def setup(): # executed on each node before jobs are scheduled
 	current_fold = None
 	return 0
 
-def multi_tag(working_dir, fold_id, sentence):
-	global current_fold
-	tagger = None
-
-	if fold_id != current_fold:
-		tagger = MaxEntMarkovModel(feature_templates=Ratnaparkhi96Features, word_normaliser=CollinsNormalisation)
-		tagger.load(working_dir, '-fold_%02d' % fold_id)
-		current_fold = fold_id
-
-	s, _ = map(list, zip(*sentence))
-	single = tagger.tag(s)
-	multi = tagger.multi_tag(s)  # get all multi tags and then filter them later to tune ambiguity.
-	return (single, multi)
+def train(working_dir, fold_datas, fold_id, reg, mxitr):
+	tagger = MaxEntMarkovModel(feature_templates=Ratnaparkhi96Features, word_normaliser=CollinsNormalisation)
+	tagger.train(fold_datas, regularization=reg, maxiter=mxitr)
+	tagger.save(working_dir, '-fold_%02d' % fold_id)
 
 
 class MemmMultiTag(MachineLearningModule):
@@ -45,29 +36,20 @@ class MemmMultiTag(MachineLearningModule):
 		subset_size = len(training)/num_folds
 		training_tags = {}  # Output: master set of CV tags built from the leave-1-out sets
 
-		# Tag the 1-left-out folds
 
-		cluster = dispy.JobCluster(multi_tag, setup=setup, reentrant=True)
+		# Train the model
+
+		cluster = dispy.JobCluster(train, setup=setup, reentrant=True)
 		http_server = dispy.httpd.DispyHTTPServer(cluster) # monitor cluster at http://localhost:8181
 		jobs = []
 		for i in range(num_folds):
-			tagging = training[i*subset_size:][:subset_size]
-			# learning = training[:i*subset_size] + training[(i+1)*subset_size:]
-			for i, sentence in enumerate(tagging):
-				job = cluster.submit(self.dir('working'), i, ambiguity, sentence)
-				job.id = i
-				jobs.append(job)
+			# tagging = training[i*subset_size:][:subset_size]
+			learning = training[:i*subset_size] + training[(i+1)*subset_size:]
+			job = cluster.submit(self.dir('working'), learning, i, reg, mxitr)
+			job.id = i
+			jobs.append(job)
 		cluster.wait() # wait for all jobs to finish
 		http_server.shutdown() # this waits until browser gets all updates
 		cluster.close()
 
-		for job in jobs:
-			job()
-			if job.status != dispy.DispyJob.Finished:
-				print('job %s failed: %s' % (job.id, job.exception))
-			else:
-				print('%s: %s' % (job.id, job.result))
-				training_tags.append(job.result)
-
-		self.backup(training_tags, self.dir('working') + '/memm_tagged_sentences-reg_%.2f.pickle' % self.get('regularization'))
-		return False
+		return False  # no saving this module.
